c7ee7c2d9b218caa9bb5a309f15be427a770c30e
Raktimb98 <raktimb181@gmail.com>
2025-09-24 19:59:12 +0530

    Refactor variable declarations and add type annotations in TypeScript file

--- variables.js
+++ variables.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greeting = "Hello I'm a TypeScript file!";
+var age = 20;
+greeting.toLowerCase();
 console.log(greeting);
+//*Number
+//NOTE:JavaScript does not have a special runtime value for integers, so there’s no equivalent to int or float - everything is simply number
+var userId = 2154.3;
+//* Boolean
+var isLoggedIn = false;
--- variables.ts
+++ variables.ts
@@ -1,5 +1,12 @@
-let greeting:string = "Hello I'm a TypeScript file!";
-let age:number = 20;
+let greeting: string = "Hello I'm a TypeScript file!";
+let age: number = 20;
 greeting.toLowerCase();
 console.log(greeting);
+
+//*Number
+//NOTE:JavaScript does not have a special runtime value for integers, so there’s no equivalent to int or float - everything is simply number
+let userId: number = 2154.3;
+
+//* Boolean
+let isLoggedIn: boolean = false;
 export {};
