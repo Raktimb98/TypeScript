80aba8671a4f106ac6717a0b0352991eaa5b7040
Raktimb98 <raktimb181@gmail.com>
2025-09-26 19:14:14 +0530

    Add TypeScript implementation of functions with type annotations

---
+++ functions.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//* Part I
+function multiply(a, b) {
+    return a * b;
+}
+var c = multiply(2, 3);
+console.log(c);
+function signUpUser(name, email, isPaid) { }
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+signUpUser("frostFern", "frostfern@xyz.com", true);
---
+++ functions.ts
@@ -0,0 +1,14 @@
+//* Part I
+function multiply(a: number,b: number){
+    return a * b;
+}
+let c = multiply(2,3);
+console.log(c);
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+signUpUser("frostFern","frostfern@xyz.com",true)
+
+
+export {};
