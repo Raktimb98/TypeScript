f6e57942a43b6793b3d2ad706a65ed35d7e6d9ae
Raktimb98 <raktimb181@gmail.com>
2025-09-27 19:19:02 +0530

    Refactor TypeScript functions to include return types and improve type safety

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //* Part I
 function multiply(a, b) {
     return a * b;
+    // return "hello" //* Error: Type 'string' is not assignable to type 'number'.
 }
 var c = multiply(2, 3);
 console.log(c);
@@ -11,3 +12,20 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 signUpUser("frostFern", "frostfern@xyz.com", true);
+//* Part II
+function getValue(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return "200 OK";
+}
+var cars = ["Ford", "Toyota", "Honda"];
+cars.map(function (cars) {
+    return "This is a ".concat(cars);
+});
+function consoleError(errMsg) {
+    console.log(errMsg);
+}
+function handleError(errMsg) {
+    throw new Error(errMsg);
+}
--- functions.ts
+++ functions.ts
@@ -1,6 +1,7 @@
 //* Part I
-function multiply(a: number,b: number){
+function multiply(a: number,b: number):number{
     return a * b;
+    // return "hello" //* Error: Type 'string' is not assignable to type 'number'.
 }
 let c = multiply(2,3);
 console.log(c);
@@ -10,5 +11,25 @@ function signUpUser(name: string, email: string, isPaid: boolean){}
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
 signUpUser("frostFern","frostfern@xyz.com",true)
 
+//* Part II
+
+function getValue(myVal:number):string | boolean{
+    if(myVal > 5){
+        return true;
+    }
+    return "200 OK";
+}
+
+const cars = ["Ford","Toyota","Honda"];
+cars.map((cars):string =>{
+    return `This is a ${cars}`;
+})
+
+function consoleError(errMsg:string):void{
+    console.log(errMsg);
+}
+function handleError(errMsg:string):never{
+    throw new Error(errMsg);
+}
 
 export {};
