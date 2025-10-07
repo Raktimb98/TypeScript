e5a88c63fadad3f4f85fb8d72342d256fbafbb94
Raktimb98 <raktimb181@gmail.com>
2025-10-07 15:39:23 +0530

    Refactor type aliases and user object structure in TypeScript implementation

---
+++ typeAliases.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//* Simple way to understand type aliases
+var a = "Hello";
+var b = "Hello";
+var c = "Hello";
+var d = "Hello";
+var e = "Hello";
+var f = "Hello";
+var g = "Hello";
+var h = "Hello";
+var myUser = {
+    _id: "1234",
+    name: "John",
+    email: "john@xyz.dev",
+    isActive: true
+};
+console.log(myUser.name);
--- typeAliases.ts
+++ typeAliases.ts
@@ -1,13 +1,3 @@
-type User = {
-    name: string;
-    age: number;
-    isActive: boolean;
-}
-function getUserInfo(user:User){
-
-}
-getUserInfo({name:"Alice", age:30, isActive:true});
-
 //* Simple way to understand type aliases
 let a: string | number | undefined = "Hello";
 let b: string | number | undefined = "Hello";
@@ -19,3 +9,22 @@ let e: StringNumberUndefined = "Hello";
 let f: StringNumberUndefined = "Hello";
 let g: StringNumberUndefined = "Hello";
 let h: StringNumberUndefined = "Hello";
+
+//* Type alias with object types
+type User = {
+    readonly _id: string;
+    name: string;
+    email: string;
+    isActive: boolean;
+    upiDetails?:string; //optional property
+}
+let myUser: User = {
+    _id: "1234",
+    name: "John",
+    email: "john@xyz.dev",
+    isActive: true
+}
+
+console.log(myUser.name);
+
+export { }
