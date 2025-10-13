2b4fa5a6e63928c69001e0a6103eedfeff9e4d47
Raktimb98 <raktimb181@gmail.com>
2025-10-13 18:32:17 +0530

    Refactor function definitions and variable assignments in unionTypes.js

--- unionTypes.js
+++ unionTypes.js
@@ -1,7 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function myId(id) {
-    console.log("ID: ".concat(id));
+// function myId(id:number|string){
+//     console.log(`ID: ${id}`);
+// }
+// myId(1);
+// myId("200");
+// export {}
+var FrostFern = {
+    name: "Frost",
+    id: 123
+};
+FrostFern = {
+    username: "Ff",
+    id: 456
+};
+//Another Example
+function getId(id) {
+    if (typeof id === "string") {
+        return id.toLowerCase();
+    }
+    return id;
 }
-myId(1);
-myId("200");
+console.log("Db ID: ".concat(getId(123)));
