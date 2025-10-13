122a4a1dd017fe1a8594fd17557f78a8270bc32e
Raktimb98 <raktimb181@gmail.com>
2025-10-13 18:31:37 +0530

    Refactor union types and user object structure in TypeScript implementation

--- unionTypes.ts
+++ unionTypes.ts
@@ -1,6 +1,34 @@
-function myId(id:number|string){
-    console.log(`ID: ${id}`);
+// function myId(id:number|string){
+//     console.log(`ID: ${id}`);
+// }
+// myId(1);
+// myId("200");
+// export {}
+
+type User = {
+    name: string;
+    id: number;
 }
-myId(1);
-myId("200");
-export {}
+type Admin = {
+    username: string;
+    id: number;
+}
+
+let FrostFern: User | Admin = {
+    name: "Frost",
+    id: 123
+}
+FrostFern = {
+    username: "Ff",
+    id: 456
+}
+
+//Another Example
+
+function getId(id: number | string) {
+    if (typeof id === "string") {
+        return id.toLowerCase();
+    }
+    return id;
+}
+console.log(`Db ID: ${getId(123)}`);
