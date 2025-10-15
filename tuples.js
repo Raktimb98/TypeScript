d29133fb99a8e58c5d772a03dd30a774f4ef1b73
Raktimb98 <raktimb181@gmail.com>
2025-10-15 19:32:19 +0530

    Add tuples implementation with user and RGB definitions in tuples.js and tuples.ts

---
+++ tuples.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const user: (string | number)[] = [1, "Alice", 2, "Bob", 3, "Charlie"];
+var user;
+user = ["FrostFern", 20, true];
+// user=[43,"FrostFern",false];//error for order
+var rgb = [255, 0, 0]; // RGB for red
+var newUser = [1, "Alice"];
+// let newUser[1] = 23; // Error: Type 'number' is not assignable to type 'string'
+newUser[1] = "FrostFern";
+// newUser.push(true); // Not allowed: Argument of type 'true' is not assignable to parameter of type 'string'.
+console.log(newUser[0]); // Output: 1
+console.log(newUser[1]); // Output: FrostFern
