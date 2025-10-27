0e36c223486bee71f1bdd949d461be7f54123323
Raktimb98 <raktimb181@gmail.com>
2025-10-27 18:18:35 +0530

    feat: enhance User class to include city as a read-only property

--- class.js
+++ class.js
@@ -1,11 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var User = /** @class */ (function () {
-    function User(name, age) {
+    function User(name, age, city) {
         this.name = name;
         this.age = age;
+        this.city = city;
         this.name = name;
         this.age = age;
     }
     return User;
 }());
-var user1 = new User("Alice", 30);
-console.log(user1);
+var user1 = new User("Alice", 30, "Los Angeles");
+// user1.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
+console.log(user1.name);
--- class.ts
+++ class.ts
@@ -1,10 +1,11 @@
-class User{
-    constructor(public name: string, public age: number){
+class User {
+    constructor(public name: string, public age: number, public readonly city: string) {
         this.name = name;
         this.age = age;
     }
 }
-const user1 = new User("Alice", 30);
+const user1 = new User("Alice", 30, "Los Angeles");
+// user1.city = "New York"; // Error: Cannot assign to 'city' because it is a read-only property.
 console.log(user1.name);
 
-export {};
+export { };
